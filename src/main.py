12c1db954cd1ecb03d23cd56c469ddc3de274372
Christopher Jenkins <topher.r.jenkins@gmail.com>
2023-06-01 10:00:25 -0700

    Learning a little out data reading and displaying

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
 import pandas as pd
-import json, os, requests
 
+
+df = pd.read_csv("data/pokemon.csv")
+search_input = input("Which Pokemon do you want to see the data for? ")
+matching_row = df.loc[df['name'] == search_input.capitalize()]
+
+result = matching_row.iloc[0].to_dict()
+
+def print_results(pokemon):
+    print("Number " + str(result['#']) + ": " + result['name'])
+    if pd.isna(result['type_2']) == True:
+        print("Typing: " + result['type_1'])
+    else:
+        print("Typing: " + result['type_1'] + "/" + result['type_2'])
+print_results(result)
